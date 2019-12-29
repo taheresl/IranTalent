10cd6649b46b63c06da9a23f5112ba5744d8cb3a
tahere_sl <t.salemi@son.ir>
2019-12-30 02:10:29 +0330

    FIRSTED commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { filter } from 'minimatch';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Talent';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+
+
+
+  }
+  value;
+  list = JSON.parse(localStorage.getItem("AllList"));
+  add() {
+
+    this.list.push({
+      id: this.list.length,
+      title: this.value,
+      checked: 0
+
+    });
+    this.value = null;
+
+    localStorage.setItem("AllList", JSON.stringify(this.list));
+  }
+  remove() {
+    for (let i = 0; i <= this.list.length - 1; i++) {
+      if (this.list[i].checked === true) {
+        this.list.splice(i, 1)
+        localStorage.setItem("AllList", JSON.stringify(this.list));
+      }
+    }
+
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatInputModule} from '@angular/material/input';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
